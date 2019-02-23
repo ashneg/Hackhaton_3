ace610a790e1839a3c74f0162cb7cf3360f00728
sambey04 <samarthh44@gmail.com>
2019-02-23 17:44:14 +0530

    all shit done

---
+++ MarkUps-HomeProperty/Home Property/index.js
@@ -0,0 +1,42 @@
+var mainText = document.getElementByid("mainText");
+var submitBtn = document.getElementByid("submitBtn");
+
+
+
+
+
+
+function submitClick()
+{
+//	var firebaseRef= firebase.database().ref();
+//	var messageText=mainText.value;
+
+//window.alert("no");
+//       firebaseRef.child("ram").set(messageText);
+
+
+//window.alert("yes");
+var rootRef = firebase.database().ref().child("seed");
+
+	rootRef.on("child_added",snap => 
+		{
+		var seed_name=snap.child("seed_name").val();
+			var seed_mos= snap.child("seed_mos").val();
+		
+console.log(seed_name);
+
+    //   $("#table_body").append("   " +seed_name + "  "+seed_mos+  )
+
+			
+	window.alert("seed found=  "+ seed_name +  "  seed Moisture= "+ seed_mos );
+
+		}
+	
+	
+	
+	
+	);
+
+
+
+}
