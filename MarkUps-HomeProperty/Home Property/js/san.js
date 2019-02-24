7592ae192e3ebf183f1a385ef5b17b8896e16299
kilzol007 <500060226@stu.upes.ac.in>
2019-02-24 07:39:55 +0530

    signup

---
+++ MarkUps-HomeProperty/Home Property/js/san.js
@@ -0,0 +1,14 @@
+
+
+function signup(){
+	  var Email = document.getElementById("email").value;
+  var Pass = document.getElementById("pass").value;
+  
+ firebase.auth().createUserWithEmailAndPassword(Email, Pass).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+	  alert("Errors");
+  }
